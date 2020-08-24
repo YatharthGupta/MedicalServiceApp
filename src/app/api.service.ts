2e617281642e852828a53d2bc992d540d3074dab
yjain <yatharth@igt.in>
2020-08-24 11:26:26 +0530

    https://github.com/YatharthGupta/MedicalServiceApp.git

---
+++ src/app/api.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from "@angular/common/http";
+import { throwError } from 'rxjs';
+import { retry, catchError, tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  public API_BASE_ADDRESS = 'https://api.inquickerstaging.com/v3/winter.inquickerstaging.com';
+  public SERVICES_URL = this.API_BASE_ADDRESS + "/services";
+  public PROVIDERS_URL = this.API_BASE_ADDRESS + "/providers?include=locations&data.type='providers'";
+  
+
+  // public API_BASE_ADDRESS = 'http://localhost:3000';
+  // public SERVICES_URL = this.API_BASE_ADDRESS + "/products";
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+
+  public getServices() {
+    //  return this.httpClient.get(this.SERVICES_URL,{responseType  : 'text'}).pipe(catchError(this.handleError));
+
+    return this.httpClient.get(this.SERVICES_URL, {  params: new HttpParams({fromString: "_page=1&_limit=20"}), observe: "response"}).pipe(retry(3), catchError(this.handleError), tap(res => {
+      // console.log(res.headers.get('Link'));
+      
+    }));
+
+    // return fetch(this.SERVICES_URL, {
+    //   method: 'GET'
+  // });
+  }
+
+  public getProviders() {
+    //return this.httpClient.get(this.PROVIDERS_URL).pipe(catchError(this.handleError));
+
+    return this.httpClient.get(this.PROVIDERS_URL, {  params: new HttpParams({fromString: "_page=1&_limit=20"}), observe: "response"}).pipe(retry(3), catchError(this.handleError), tap(res => {
+      //console.log(res.headers.get('Link'));
+    }));
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ApiService } from '../app/api.service';
+import { HttpResponse } from '@angular/common/http';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
+import { Observable, Subject } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MedicalServiceApp';
+  //services1 =[];
+  services = [];
+  providers = [];
+
+  providersfiltered = [];
+  private readonly destroy$ = new Subject();
+  constructor(private apiService: ApiService) { }
+  public activeElement = 1;
+  ngOnInit() {
+
+    this.apiService.getServices().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.services = res.body.data;
+      //this.services = this.services1.map(data => _.uniqBy(data,'attributes'));
+
+    })
+
+    this.apiService.getProviders().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.providers = res.body.data;
+    })
+   
+    //this.providersfiltered = this.providers.filter(element => element.attribute.subspecialties = this.activeElement);
+    
+  }
+  public selectedItem(id) {
+    this.activeElement = id;
+  }
+
+
+
+  // this.apiService.getProviders().subscribe((data: any[]) => {
+  //   console.log(data);
+  //   this.providers = data;
+  // })
+
+
 }
